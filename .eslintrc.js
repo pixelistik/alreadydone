83f7c93a2a1e6ce02bf83d54ddf3105e79abe533
Pixelistik <code@pixelistik.de>
2016-01-09 17:07:08 +0100

    ESlint rules

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "rules": {
+        "indent": [
+            2,
+            4
+        ],
+        "quotes": [
+            2,
+            "double"
+        ],
+        "linebreak-style": [
+            2,
+            "unix"
+        ],
+        "semi": [
+            2,
+            "always"
+        ]
+    },
+    "env": {
+        "browser": true,
+        "node": true,
+        "mocha": true
+    },
+    "extends": "eslint:recommended"
+};
