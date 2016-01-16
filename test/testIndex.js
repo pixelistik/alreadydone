b9cd5acaaf55baaac93f859699dc1fcbdb244dc5
Pixelistik <code@pixelistik.de>
2016-01-16 21:06:41 +0100

    Test for simple save/reload of a List
    Gotcha: there is no test DB separation yet, running the tests will drop
    the entire collection before each test.

--- test/testIndex.js
+++ test/testIndex.js
@@ -1,6 +1,9 @@
 "use strict";
 
 var request = require("supertest");
+var mongodb = require("mongodb");
+
+var mongoUrl = "mongodb://mongodb:27017/myproject";
 
 var app = require("../js/server/app.js");
 
@@ -9,5 +12,55 @@ describe("App backend server", function () {
         request(app)
             .get("/index.html")
             .expect(200, done);
+    });
+
+    describe("List", function () {
+        describe("Saving", function () {
+            var data = {
+                _id: "my-test-id",
+                tasks: [
+                    {
+                        title: "One",
+                        done: false
+                    },
+                    {
+                        title: "two",
+                        done: true
+                    }
+                ]
+            };
+
+            beforeEach(function () {
+                mongodb.MongoClient.connect(mongoUrl, function (err, db) {
+                    var collection = db.collection("documents");
+
+                    collection.drop();
+
+                    db.close();
+                });
+            });
+
+            it("should accept the request", function (done) {
+                request(app)
+                    .put("/list/my-test-id")
+                    .set("Accept", "application/json")
+                    .send(data)
+                    .expect(200, done);
+            });
+
+            it("should save and reload the data", function (done) {
+                request(app)
+                    .put("/list/my-test-id")
+                    .set("Accept", "application/json")
+                    .send(data)
+                    .end(function (err, res) {
+                        request(app)
+                            .get("/list/my-test-id")
+                            .set("Accept", "application/json")
+                            .expect(JSON.stringify(data))
+                            .expect(200, done);
+                    });
+            });
+        });
     });
 });
