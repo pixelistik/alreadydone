5348595a6cb5b0d221570b3826ca1d45b16caf75
Pixelistik <code@pixelistik.de>
2016-01-16 21:09:49 +0100

    We need to GET a List, too

--- js/server/app.js
+++ js/server/app.js
@@ -11,19 +11,25 @@ var app = express();
 app.use(bodyParser.json());
 
 app.put("/list/:id", function (request, response) {
-    console.log(request.body);
-    console.log("PUT for " + request.params.id);
-
     mongodb.MongoClient.connect(mongoUrl, function(err, db) {
         var collection = db.collection("documents");
 
         collection.insertOne(
-            request.body
-        , function(err, result) {
-            console.log(err);
-            console.log("Inserted document  into the document collection");
-            db.close();
-            response.sendStatus(200);
+            request.body,
+            function (err, result) {
+                db.close();
+                response.sendStatus(200);
+            }
+        );
+    });
+});
+
+app.get("/list/:id", function (request, response) {
+    mongodb.MongoClient.connect(mongoUrl, function(err, db) {
+        var collection = db.collection("documents");
+
+        collection.findOne({_id: request.params.id}, function (err, items) {
+            response.send(items);
         });
 
     });
