e32b697c2a0454b1be03e374adc29689a1ed71eb
Pixelistik <code@pixelistik.de>
2016-01-24 12:04:32 +0100

    Change API to modify tasks only
    Entire lists are available as read-only, PUT only for single tasks.
    
    This makes updates more atomic and removes merging complexity.

--- js/server/app.js
+++ js/server/app.js
@@ -11,7 +11,7 @@ var app = express();
 app.use(bodyParser.json());
 
 app.put("/list/:id", function (request, response) {
-    mongodb.MongoClient.connect(mongoUrl, function(err, db) {
+    mongodb.MongoClient.connect(mongoUrl, function (err, db) {
         var collection = db.collection("documents");
 
         collection.updateOne(
@@ -30,11 +30,11 @@ app.put("/list/:id", function (request, response) {
     });
 });
 
-app.get("/list/:id", function (request, response) {
-    mongodb.MongoClient.connect(mongoUrl, function(err, db) {
+app.get("/list/:id/tasks", function (request, response) {
+    mongodb.MongoClient.connect(mongoUrl, function (err, db) {
         var collection = db.collection("documents");
 
-        collection.findOne({_id: request.params.id}, function (err, items) {
+        collection.find({listId: request.params.id}).toArray(function (err, items) {
             if(!err) {
                 response.send(items);
             }
@@ -43,6 +43,61 @@ app.get("/list/:id", function (request, response) {
 
     });
 });
+
+app.get("/task/:id", function (request, response) {
+    mongodb.MongoClient.connect(mongoUrl, function (err, db) {
+        var collection = db.collection("documents");
+
+        collection.findOne({_id: request.params.id}, {}, function (err, task) {
+            if(err) {
+                response.status(500).send(err);
+                return;
+            }
+
+            if (task) {
+                response.send(task);
+            } else {
+                response.sendStatus(404);
+            }
+        });
+    });
+});
+
+app.put("/task/:id", function (request, response) {
+    if (
+        !request.body._id ||
+        !request.body.listId ||
+        !request.body.modified
+    ) {
+        response.status(400).send("Properties _id, modified, and listId are mandatory.");
+        return;
+    }
+
+    mongodb.MongoClient.connect(mongoUrl, function (err, db) {
+        var collection = db.collection("documents");
+
+        collection.findOne({_id: request.body._id}, {}, function (err, task) {
+            if (task && task.modified > request.body.modified) {
+                response.status(409).send("Data on server is newer.");
+            } else {
+                collection.updateOne(
+                    {_id: request.body._id},
+                    request.body,
+                    {upsert:true},
+                    function (err, result) {
+                        db.close();
+                        if(!err) {
+                            response.sendStatus(200);
+                        } else {
+                            response.status(500).send(err);
+                        }
+                    }
+                );
+            }
+        });
+    });
+});
+
 
 app.use(express.static("."));
 
--- test/testIndex.js
+++ test/testIndex.js
@@ -1,5 +1,6 @@
 "use strict";
 
+var assert = require("chai").assert;
 var request = require("supertest");
 var mongodb = require("mongodb");
 
@@ -14,69 +15,291 @@ describe("App backend server", function () {
             .expect(200, done);
     });
 
-    describe("List", function () {
-        describe("Saving", function () {
-            var data = {
-                _id: "my-test-id",
-                tasks: [
+    var tasksFixture = [
+        {
+            _id: "1",
+            id: "1",
+            listId: "test-list",
+            title: "one",
+            done: false,
+            modified: 100
+        },
+        {
+            _id: "2",
+            id: "2",
+            listId: "test-list",
+            title: "two",
+            done: true,
+            modified: 200
+        },
+        {
+            _id: "3",
+            id: "3",
+            listId: "different-test-list",
+            title: "three",
+            done: true,
+            modified: 210
+        }
+    ];
+
+    beforeEach(function (done) {
+        mongodb.MongoClient.connect(mongoUrl, function (err, db) {
+            var collection = db.collection("documents");
+
+            collection.drop(function (err, reply) {
+                collection.insert(tasksFixture, null, function (err, result) {
+                    db.close();
+                    done();
+                });
+            });
+        });
+    });
+
+    describe("Lists", function () {
+        describe("GET the tasks for a list", function () {
+            it("should return tasks for the first list", function (done) {
+                var expectedTasks = [
                     {
-                        title: "One",
-                        done: false
+                        _id: "3",
+                        id: "3",
+                        listId: "different-test-list",
+                        title: "three",
+                        done: true,
+                        modified: 210
+                    }
+                ];
+
+                request(app)
+                   .get("/list/different-test-list/tasks")
+                   .set("Accept", "application/json")
+                   .expect(JSON.stringify(expectedTasks))
+                   .expect(200, done);
+            });
+
+            it("should return all tasks for the second list", function (done) {
+                var expectedTasks = [
+                    {
+                        _id: "1",
+                        id: "1",
+                        listId: "test-list",
+                        title: "one",
+                        done: false,
+                        modified: 100
                     },
                     {
+                        _id: "2",
+                        id: "2",
+                        listId: "test-list",
                         title: "two",
-                        done: true
+                        done: true,
+                        modified: 200
                     }
-                ]
-            };
+                ];
 
-            beforeEach(function () {
-                mongodb.MongoClient.connect(mongoUrl, function (err, db) {
-                    var collection = db.collection("documents");
+                request(app)
+                   .get("/list/test-list/tasks")
+                   .set("Accept", "application/json")
+                   .expect(JSON.stringify(expectedTasks))
+                   .expect(200, done);
+            });
+        });
 
-                    collection.drop();
+        describe("Tasks", function () {
+            describe("GET a task", function () {
+                it("should GET the first task", function (done) {
+                    request(app)
+                       .get("/task/1")
+                       .set("Accept", "application/json")
+                       .expect(JSON.stringify(tasksFixture[0]))
+                       .expect(200, done);
+                });
 
-                    db.close();
+                it("should GET the third task", function (done) {
+                    request(app)
+                       .get("/task/3")
+                       .set("Accept", "application/json")
+                       .expect(JSON.stringify(tasksFixture[2]))
+                       .expect(200, done);
+                });
+
+                it("should 404 on an unknown task", function (done) {
+                    request(app)
+                       .get("/task/some-unknown-task")
+                       .set("Accept", "application/json")
+                       .expect(404, done);
                 });
             });
 
-            it("should accept the request", function (done) {
-                request(app)
-                    .put("/list/my-test-id")
-                    .set("Accept", "application/json")
-                    .send(data)
-                    .expect(200, done);
-            });
+            describe("PUT a task", function () {
+                it("should accept a new task", function (done) {
+                    request(app)
+                       .put("/task/a-new-task")
+                       .set("Accept", "application/json")
+                       .send({
+                           _id: "a-new-task",
+                           id: "a-new-task",
+                           listId: "some-list",
+                           title: "some new task",
+                           done: false,
+                           modified: 100
+                       })
+                       .expect(200)
+                       .end(function (err, res) {
+                           if (err) {
+                               return done(err);
+                           }
 
-            it("should save and reload the data", function (done) {
-                request(app)
-                    .put("/list/my-test-id")
-                    .set("Accept", "application/json")
-                    .send(data)
-                    .end(function (err, res) {
-                        request(app)
-                            .get("/list/my-test-id")
-                            .set("Accept", "application/json")
-                            .expect(JSON.stringify(data))
-                            .expect(200, done);
-                    });
-            });
+                           mongodb.MongoClient.connect(mongoUrl, function (err, db) {
+                               var collection = db.collection("documents");
 
-            it("should accept update requests (upserts)", function (done) {
-                request(app)
-                    .put("/list/my-test-id")
-                    .set("Accept", "application/json")
-                    .send(data)
-                    .expect(200, function () {
-                        // Request again
-                        request(app)
-                            .put("/list/my-test-id")
-                            .set("Accept", "application/json")
-                            .send(data)
-                            .expect(200, done);
-                    });
+                               collection.findOne({_id: "a-new-task"}, {}, function (err, document) {
+                                   assert.equal(document.title, "some new task");
+                                   done();
+                               });
+                           });
+                       });
+                });
 
+                it("should accept an updated task with younger modification", function (done) {
+                    request(app)
+                       .put("/task/a-new-task")
+                       .set("Accept", "application/json")
+                       .send({
+                           _id: "1",
+                           id: "1",
+                           listId: "test-list",
+                           title: "one (newer)",
+                           done: true,
+                           modified: 500
+                       })
+                       .expect(200)
+                       .end(function (err, res) {
+                           if (err) {
+                               return done(err);
+                           }
 
+                           mongodb.MongoClient.connect(mongoUrl, function (err, db) {
+                               var collection = db.collection("documents");
+
+                               collection.findOne({_id: "1"}, {}, function (err, document) {
+                                   assert.equal(document.title, "one (newer)");
+                                   done();
+                               });
+                           });
+                       });
+                });
+
+                it("should refuse an updated task with older modification", function (done) {
+                    request(app)
+                       .put("/task/a-new-task")
+                       .set("Accept", "application/json")
+                       .send({
+                           _id: "1",
+                           id: "1",
+                           listId: "test-list",
+                           title: "one (older)",
+                           done: true,
+                           modified: 10
+                       })
+                       .expect(409)
+                       .end(function (err, res) {
+                           if (err) {
+                               return done(err);
+                           }
+
+                           mongodb.MongoClient.connect(mongoUrl, function (err, db) {
+                               var collection = db.collection("documents");
+
+                               collection.findOne({_id: "1"}, {}, function (err, document) {
+                                   assert.equal(document.title, "one");
+                                   done();
+                               });
+                           });
+                       });
+                });
+
+                it("should refuse a task with missing ID", function (done) {
+                    request(app)
+                       .put("/task/1")
+                       .set("Accept", "application/json")
+                       .send({
+                           listId: "test-list",
+                           title: "missing ID",
+                           done: false,
+                           modified: 10
+                       })
+                       .expect(400)
+                       .end(function (err, res) {
+                           if (err) {
+                               return done(err);
+                           }
+
+                           mongodb.MongoClient.connect(mongoUrl, function (err, db) {
+                               var collection = db.collection("documents");
+
+                               collection.findOne({title: "missing ID"}, {}, function (err, document) {
+                                   assert.equal(document, null);
+                                   done();
+                               });
+                           });
+                       });
+                });
+
+                it("should refuse a task with missing list ID", function (done) {
+                    request(app)
+                       .put("/task/1")
+                       .set("Accept", "application/json")
+                       .send({
+                           _id: "1",
+                           id: "1",
+                           title: "missing list ID",
+                           done: false,
+                           modified: 1000
+                       })
+                       .expect(400)
+                       .end(function (err, res) {
+                           if (err) {
+                               return done(err);
+                           }
+
+                           mongodb.MongoClient.connect(mongoUrl, function (err, db) {
+                               var collection = db.collection("documents");
+
+                               collection.findOne({_id: "1"}, {}, function (err, document) {
+                                   assert.equal(document.title, "one");
+                                   done();
+                               });
+                           });
+                       });
+                });
+
+                it("should refuse a task without modified timestamp", function (done) {
+                    request(app)
+                       .put("/task/1")
+                       .set("Accept", "application/json")
+                       .send({
+                           _id: "1",
+                           id: "1",
+                           listId: "test-list",
+                           title: "missing modified",
+                           done: false
+                       })
+                       .expect(400)
+                       .end(function (err, res) {
+                           if (err) {
+                               return done(err);
+                           }
+
+                           mongodb.MongoClient.connect(mongoUrl, function (err, db) {
+                               var collection = db.collection("documents");
+
+                               collection.findOne({_id: "1"}, {}, function (err, document) {
+                                   assert.equal(document.title, "one");
+                                   done();
+                               });
+                           });
+                       });
+                });
             });
         });
     });
